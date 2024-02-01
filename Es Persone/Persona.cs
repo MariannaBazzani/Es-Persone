fa3e8b9c3d6543bb9048f866c979356d0d2582af
MariannaBazzani <merionbaz@gmail.com>
2024-02-01 17:43:33 +0100

    Aggiungere i file di progetto.

---
+++ Es Persone/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Persone
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona() { }
+        public Persona(string nome, int eta)
+        {
+            this.Nome = nome;
+            this.Eta = eta;
+        }
+
+        public static Persona Parse(string str)
+        {
+            string[] caratteristiche = str.Split(',');
+            string nome = caratteristiche[0];
+            int eta = int.Parse(caratteristiche[1]);
+
+            return new Persona(nome, eta);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", Nome, Eta);
+        }
+    }
+}
---
+++ Es Persone/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Persone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> Persone = new List<Persona>();
+
+            Console.WriteLine("Inserisci il numero delle persone che vuoi aggiungere alla lista: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Inserisci caratteristiche persona: ");
+                Persona p = Persona.Parse(Console.ReadLine());
+                Persone.Add(p);
+            }
+
+            foreach(var Persona in Persone)
+            {
+                if (Persona.Eta >= 18)
+                    Console.WriteLine(Persona.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
